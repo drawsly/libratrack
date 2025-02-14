8893600bbdbb28b79b722306617d6ce3a24af7f8
Enes Yasin Divrengi <dr4wsly@gmail.com>
2025-02-14 20:13:03 +0300

    feat: Add ThemeProvider component and site metadata, and implement utility function for class merging

---
+++ src/lib/metadata.ts
@@ -0,0 +1,5 @@
+export const siteMetadata = {
+    title: "LibraTrack",
+    description:
+        "LibraTrack, kütüphanelerin kitap yönetimi, ödünç alma ve iade işlemlerini kolaylaştıran modern bir yönetim sistemidir.",
+};
