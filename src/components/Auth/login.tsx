8ce1bd158dd1f2e1bbdac22cb6db8dd395f7520a
Enes Yasin Divrengi <dr4wsly@gmail.com>
2025-02-15 19:03:39 +0300

    feat: Refactor login component to manage email and password state with controlled inputs

--- src/components/Auth/login.tsx
+++ src/components/Auth/login.tsx
@@ -12,37 +12,51 @@ import {
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { useState } from 'react';
 
 export default function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div className="grid w-full h-screen grow items-center px-4 sm:justify-center">
       <Card className="w-full sm:w-96">
         <CardHeader>
-          <CardTitle>LibraTrack'e giriş yap.</CardTitle>
+          <CardTitle>LibraTrack&apos;e giriş yap.</CardTitle>
           <CardDescription>
             Tekrar hoşgeldiniz! Lütfen devam etmek için giriş yapın.
           </CardDescription>
         </CardHeader>
-        <CardContent className="grid gap-y-4">
-          <div className="space-y-2">
-            <Label>Eposta adresi</Label>
-            <Input type="email" required />
-          </div>
-
-          <div className="space-y-2">
-            <Label>Şifre</Label>
-            <Input type="password" required />
-          </div>
-        </CardContent>
-
-        <CardFooter>
-          <div className="grid w-full gap-y-4">
-            <Button>Giriş yap</Button>
-            <Button variant="link" size="sm" asChild>
-              <Link href="/register">Hesabın yok mu? Kayıt ol</Link>
-            </Button>
-          </div>
-        </CardFooter>
+        <form action={(formData) => console.log(formData)}>
+          <CardContent className="grid gap-y-4">
+            <div className="space-y-2">
+              <Label>Eposta adresi</Label>
+              <Input
+                type="email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="space-y-2">
+              <Label>Şifre</Label>
+              <Input
+                type="password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <div className="grid w-full gap-y-4">
+              <Button>Giriş yap</Button>
+              <Button variant="link" size="sm" asChild>
+                <Link href="/register">Hesabın yok mu? Kayıt ol</Link>
+              </Button>
+            </div>
+          </CardFooter>
+        </form>
       </Card>
     </div>
   );
