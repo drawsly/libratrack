f010064178b9407eb0c97fd150d18ed6e19e807b
Enes Yasin Divrengi <dr4wsly@gmail.com>
2025-02-16 13:58:29 +0300

    feat: Extend RegisterState type to include success flag and user details

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,5 +1,14 @@
 export type RegisterState = {
     error?: string;
+    success?: boolean;
+    user?: {
+        name: string | null;
+        surname: string | null;
+        email: string;
+        password: string;
+        id: number;
+        image: string | null;
+    };
 };
 
 export type LoginState = {
