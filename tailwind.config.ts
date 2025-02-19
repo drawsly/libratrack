cdf99f50446cfcba5058cadba9b1561da3966b49
Enes Yasin Divrengi <dr4wsly@gmail.com>
2025-02-19 17:41:24 +0300

    feat: Update global styles and add sidebar color variables for improved theming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ export default {
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+			screens: {
+				'2xl': '1400px'
+			}
+		},
+
 		extend: {
 			colors: {
 				background: 'hsl(var(--background))',
@@ -54,28 +62,39 @@ export default {
 					'3': 'hsl(var(--chart-3))',
 					'4': 'hsl(var(--chart-4))',
 					'5': 'hsl(var(--chart-5))'
+				},
+				sidebar: {
+					DEFAULT: 'hsl(var(--sidebar-background))',
+					foreground: 'hsl(var(--sidebar-foreground))',
+					primary: 'hsl(var(--sidebar-primary))',
+					'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+					accent: 'hsl(var(--sidebar-accent))',
+					'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+					border: 'hsl(var(--sidebar-border))',
+					ring: 'hsl(var(--sidebar-ring))'
 				}
 			},
 			animation: {
-				aurora: "aurora 60s linear infinite",
+				aurora: 'aurora 60s linear infinite'
 			},
 			keyframes: {
 				aurora: {
 					from: {
-						backgroundPosition: "50% 50%, 50% 50%",
+						backgroundPosition: '50% 50%, 50% 50%'
 					},
 					to: {
-						backgroundPosition: "350% 50%, 350% 50%",
-					},
-				},
+						backgroundPosition: '350% 50%, 350% 50%'
+					}
+				}
 			},
 			borderRadius: {
 				lg: 'var(--radius)',
 				md: 'calc(var(--radius) - 2px)',
 				sm: 'calc(var(--radius) - 4px)'
-			},
+			}
 		}
 	},
+
 	plugins: [require("tailwindcss-animate"), addVariablesForColors],
 } satisfies Config;
 
