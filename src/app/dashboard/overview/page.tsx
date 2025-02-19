88bb19d47f65afd11951ce4332f061aa2bf4a1e2
Enes Yasin Divrengi <dr4wsly@gmail.com>
2025-02-19 17:42:25 +0300

    feat: Add OverViewPage component with PageContainer integration

---
+++ src/app/dashboard/overview/page.tsx
@@ -0,0 +1,9 @@
+import PageContainer from "@/components/layout/page-container";
+
+export default function OverViewPage() {
+    return (
+        <PageContainer>
+            Genel Bakış Sayfası
+        </PageContainer>
+    );
+}
