6bbb5f37013149907402935c409f394705d765b2
Enes Yasin Divrengi <dr4wsly@gmail.com>
2025-02-19 17:41:43 +0300

    feat: Add DashboardLayout component with sidebar and header integration

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,26 @@
+import KBar from '@/components/kbar';
+import AppSidebar from '@/components/layout/app-sidebar';
+import Header from '@/components/layout/header';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+import { cookies } from 'next/headers';
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get('sidebar:state')?.value === 'true';
+
+  return (
+    <KBar>
+      <SidebarProvider defaultOpen={defaultOpen}>
+        <AppSidebar />
+        <SidebarInset>
+          <Header />
+          {children}
+        </SidebarInset>
+      </SidebarProvider>
+    </KBar>
+  );
+}
