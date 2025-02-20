4a20dd24d683e1884042c90e5c08b49df787e32f
Enes Yasin Divrengi <dr4wsly@gmail.com>
2025-02-20 17:41:21 +0300

    feat: Enhance overview page to display user session information and welcome message

--- src/app/dashboard/overview/page.tsx
+++ src/app/dashboard/overview/page.tsx
@@ -1,9 +1,19 @@
-import PageContainer from "@/components/layout/page-container";
+import { auth } from '@/auth';
+import PageContainer from '@/components/layout/page-container';
 
-export default function OverViewPage() {
-    return (
-        <PageContainer>
-            Genel Bakış Sayfası
-        </PageContainer>
-    );
+export default async function OverViewPage() {
+  const session = await auth();
+
+  return (
+    <PageContainer>
+      <div className="space-y-4">
+        <h1 className="text-2xl font-bold">
+          Hoş Geldiniz, {session?.user.name}
+        </h1>
+        <h2 className="text-1xl font-bold">Genel Bakış Sayfası</h2>
+
+        <pre>{JSON.stringify(session, null, 2)}</pre>
+      </div>
+    </PageContainer>
+  );
 }
