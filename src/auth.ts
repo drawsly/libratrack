ab823e4bd7cc979869367fba279f0db739bbb976
Enes Yasin Divrengi <dr4wsly@gmail.com>
2025-02-20 21:18:46 +0300

    feat: Update NextAuth configuration to use JWT session strategy

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,7 @@
 import NextAuth from "next-auth"
-import authConfig from "./auth.config"
+import { authConfig } from "./auth.config"
 
-export const { auth, handlers, signIn, signOut } = NextAuth(authConfig)
+export const { auth, signIn, signOut, handlers } = NextAuth({
+    ...authConfig,
+    session: { strategy: "jwt" },
+})
