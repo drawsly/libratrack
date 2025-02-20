4addce6d59ca3ea7bd217e832cad537b4ef235e3
Enes Yasin Divrengi <dr4wsly@gmail.com>
2025-02-20 17:44:09 +0300

    feat: Add redirect property to LoginState and RegisterState interfaces

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,9 +1,11 @@
 export interface LoginState {
-    error?: string;
-    success?: boolean;
+    error?: string
+    success?: boolean
+    redirect?: string
 }
 
 export interface RegisterState {
-    error?: string;
-    success?: boolean;
+    error?: string
+    success?: boolean
+    redirect?: string
 }
