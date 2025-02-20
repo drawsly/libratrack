79e82f77fbdf4b5cfbc4be3f25d1f421aee229f0
Enes Yasin Divrengi <dr4wsly@gmail.com>
2025-02-20 21:19:23 +0300

    fix: Update layout component to use absolute import for styles and enhance HTML structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,23 +4,27 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { siteMetadata } from '@/lib/metadata';
-import { ThemeProvider } from '@/components/layout/Theme/theme-provider';
 import { Toaster } from '@/components/ui/sonner';
 import NextTopLoader from 'nextjs-toploader';
 
-import '../styles/globals.css';
+import '@/styles/globals.css';
+import { ThemeProvider } from '@/components/layout/Theme/theme-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = siteMetadata;
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="tr" className={inter.className} suppressHydrationWarning>
+    <html
+      lang="tr"
+      className={`${inter.className} antialiased`}
+      suppressHydrationWarning
+    >
       <body className={`overflow-hidden`}>
         <NextTopLoader showSpinner={false} />
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
