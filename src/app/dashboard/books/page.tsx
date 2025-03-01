3c9d57e945f681acfa8ae5f0df9f8fccc3b4e6a2
Enes Yasin Divrengi <dr4wsly@gmail.com>
2025-03-01 21:44:35 +0300

    feat: Refactor Books component to utilize BooksPage and handle search parameters

--- src/app/dashboard/books/page.tsx
+++ src/app/dashboard/books/page.tsx
@@ -1,37 +1,12 @@
-import PageContainer from '@/shared/components/layout/page-container';
-import { buttonVariants } from '@/shared/components/ui/button';
-import { Heading } from '@/shared/components/ui/heading';
-import { Separator } from '@/shared/components/ui/separator';
-import { cn } from '@/shared/utils';
-import { Plus } from 'lucide-react';
-import Link from 'next/link';
-import { Suspense } from 'react';
+import BooksPage from '@/features/dashboard/books/components/books';
+import { SearchParams } from 'nuqs/server';
 
-export default function Books() {
-  return (
-    <PageContainer scrollable={false}>
-      <div className='flex flex-1 flex-col space-y-4'>
-        <div className='flex items-start justify-between'>
-          <Heading
-            title='Kitaplar'
-            description='Kitapları görüntüleyin, düzenleyin ve ekleyin.'
-          />
-          <Link
-            href='/dashboard/product/new'
-            className={cn(buttonVariants(), 'text-xs md:text-sm')}
-          >
-            <Plus className='mr-2 h-4 w-4' /> Kitap Ekle
-          </Link>
-        </div>
-        <Separator />
-        {/* <ProductTableAction /> */}
-        <Suspense
-        // key={key}
-        // fallback={<DataTableSkeleton columnCount={5} rowCount={10} />}
-        >
-          {/* <ProductListingPage /> */}
-        </Suspense>
-      </div>
-    </PageContainer>
-  );
+type pageProps = {
+  searchParams: Promise<SearchParams>;
+};
+
+export default function Books(props: pageProps) {
+  const searchParams = props.searchParams;
+
+  return <BooksPage searchParams={searchParams} />;
 }
