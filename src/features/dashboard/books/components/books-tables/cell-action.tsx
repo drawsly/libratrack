8219068244927f26a66428c84695f4a655f0da7d
Enes Yasin Divrengi <dr4wsly@gmail.com>
2025-03-03 22:22:51 +0300

    feat: Add CellAction component for managing book edit and delete actions

---
+++ src/features/dashboard/books/components/books-tables/cell-action.tsx
@@ -0,0 +1,76 @@
+'use client';
+import { AlertModal } from '@/shared/components/modal/alert-modal';
+import { Button } from '@/shared/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger
+} from '@/shared/components/ui/dropdown-menu';
+import { Book } from '../../types';
+import { Edit, MoreHorizontal, Trash } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { deleteBook } from '../../actions';
+import { toast } from 'sonner';
+
+interface CellActionProps {
+  data: Book;
+}
+
+export const CellAction: React.FC<CellActionProps> = ({ data }) => {
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+
+  const onConfirm = async () => {
+    try {
+      setLoading(true);
+      const result = await deleteBook(data.id);
+
+      if (result.success) {
+        toast.success('Kitap başarıyla silindi');
+        router.refresh();
+      } else {
+        toast.error(result.error || 'Kitap silinemedi');
+      }
+    } catch (error) {
+      toast.error('Bir hata oluştu');
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onConfirm}
+        loading={loading}
+      />
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button variant='ghost' className='h-8 w-8 p-0'>
+            <span className='sr-only'>Menüyü aç</span>
+            <MoreHorizontal className='h-4 w-4' />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align='end'>
+          <DropdownMenuLabel>İşlemler</DropdownMenuLabel>
+
+          <DropdownMenuItem
+            onClick={() => router.push(`/dashboard/books/${data.id}`)}
+          >
+            <Edit className='mr-2 h-4 w-4' /> Düzenle
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setOpen(true)}>
+            <Trash className='mr-2 h-4 w-4' /> Sil
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
