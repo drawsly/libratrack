afdf6b44e4da2881e6c3deaa5dcf6cd7e366f4f3
Enes Yasin Divrengi <dr4wsly@gmail.com>
2025-03-09 00:14:45 +0300

    feat: Add BookFormValues type for managing book form data

--- src/features/dashboard/books/types/index.ts
+++ src/features/dashboard/books/types/index.ts
@@ -14,3 +14,15 @@ export type Book = {
   entrusted: boolean | null;
   loans: Loan[];
 };
+
+export type BookFormValues = {
+  book_name: string | null;
+  author_name: string | null;
+  publisher: string | null;
+  publish_year: number | null;
+  page_count: number | null;
+  book_type: string | null;
+  book_case: string | null;
+  shelf: number | null;
+  row: number | null;
+};
